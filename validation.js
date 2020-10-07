62f3a2311d2a4efddedcc83fbb3943db09559bf4
wakennedy <alexkenned@gmail.com>
2020-10-07 14:21:23 -0700

    cleanup

--- validation.js
+++ validation.js
@@ -16,9 +16,6 @@ const loginValidation = (data) => {
   });
   return schema.validate(data);
 };
-// //VALIDATE DATA BEFORE WE CREATE
-// const { error } = schema.validate(req.body);
-// if (error) return res.status(400).send(error.details[0].message);
 
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
